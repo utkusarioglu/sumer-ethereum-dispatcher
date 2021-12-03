9b383074b61427ccd398eb0bc9626abf0aa87970
Utku Sarioglu <utkusarioglu@hotmail.com>
2021-12-03 15:58:49 +0000

    Update index.ts with kafka and events
    - Add kafka to index loop
    - Add event emitter to index loop
    - Now the app sends ethereum block events to the kafka cluster

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
 import ethereumService from "_services/ethereum/ethereum.service";
+import eeService from "_services/event-emitter/event-emitter.service";
+import kafkaService from "_services/kafka/kafka.service";
+import { NODE_ENV } from "_/config";
 
-ethereumService.listen();
+NODE_ENV === "development" && kafkaService.createTopics();
+
+kafkaService.producerConnect().then(() => {
+  console.log("producer connected");
+
+  eeService.blockNumber.sub((d) => kafkaService.sendBlockNumber(d));
+  eeService.blockContent.sub((d) => kafkaService.sendBlockContent(d));
+
+  ethereumService.listen({
+    blockNum: eeService.blockNumber.pub,
+    blockContent: eeService.blockContent.pub,
+  });
+});
