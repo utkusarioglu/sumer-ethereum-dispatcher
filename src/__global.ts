cf6fe3e3b7a7c2649035c03f0123d8dc5b37dc46
Utku Sarioglu <utkusarioglu@hotmail.com>
2021-12-07 11:32:29 +0000

    Fix typo in comment

--- src/__global.ts
+++ src/__global.ts
@@ -4,7 +4,7 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       NODE_ENV: "production" | "testing" | "development";
-      NETWORK: string; // homestad, goerli etc
+      NETWORK: string; // homestead, goerli etc
       KAFKA_BROKERS: string; // csv
       HOSTNAME: string; // provided by docker or k8s
       INFURA_API_KEY: string;
