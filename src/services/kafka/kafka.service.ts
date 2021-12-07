154af68db82117b111c46e64725e2a93091875b7
Utku Sarioglu <utkusarioglu@hotmail.com>
2021-12-07 11:32:58 +0000

    Refactor kafka.service
    - Separate producer.send logic from specific send operations to
      different topics
    - Integrate logging into the class. Use a separate method for
      handling logging.

--- src/services/kafka/kafka.service.ts
+++ src/services/kafka/kafka.service.ts
@@ -1,5 +1,6 @@
-import { Kafka, Producer } from "kafkajs";
+import { Kafka, Producer, Message } from "kafkajs";
 import { KAFKA_BROKERS, HOSTNAME } from "_/config";
+import loggerService from "_/services/logger/logger.service";
 
 /**
  * Handles kafka cluster communications
@@ -22,7 +23,7 @@ export class KafkaService {
    * @returns void promise
    */
   async producerConnect() {
-    return this.producer.connect();
+    return this.producer.connect().catch(loggerService.error);
   }
 
   /**
@@ -30,15 +31,13 @@ export class KafkaService {
    * @param blockNum block number from ethereum blockchain
    */
   async sendBlockNumber(blockNum: number) {
-    this.producer.send({
-      topic: "ethereum-block-number",
-      acks: -1,
-      messages: [
-        {
-          value: blockNum.toString(),
-        },
-      ],
-    });
+    const topic = "ethereum-block-number";
+    const messages = [
+      {
+        value: blockNum.toString(),
+      },
+    ];
+    return this.send(topic, messages);
   }
 
   /**
@@ -46,14 +45,45 @@ export class KafkaService {
    * @param blockContent block content from ethereum blockchain
    */
   async sendBlockContent<T extends Object>(blockContent: T) {
-    this.producer.send({
-      topic: "ethereum-block-content",
-      acks: -1,
-      messages: [
-        {
-          value: JSON.stringify(blockContent),
-        },
-      ],
+    const topic = "ethereum-block-content";
+    const messages = [
+      {
+        value: JSON.stringify(blockContent),
+      },
+    ];
+    return this.send(topic, messages);
+  }
+
+  /**
+   * Wrapper for kafka send. With error catcher and logger
+   * @param topic topic to send to
+   * @param messages messages to send
+   */
+  private async send(topic: string, messages: Message[]) {
+    this.producer
+      .send({
+        topic,
+        acks: -1,
+        messages,
+      })
+      .then(() => this.sendLog(topic, messages))
+      .catch(loggerService.error);
+  }
+
+  /**
+   * Logs the send operation using the loggerService
+   * @param topic kafka topic
+   * @param messages sent kafka messages
+   */
+  private sendLog(topic: string, messages: Message[]): void {
+    const stringifiedMessages = JSON.stringify(messages);
+    const maxLength = 50;
+    const conciseMessages =
+      stringifiedMessages.length > maxLength
+        ? `${stringifiedMessages.slice(0, maxLength - 3)}...`
+        : stringifiedMessages;
+    loggerService.debug(`Sent "${conciseMessages}" to "${topic}"`, {
+      section: "kafka",
     });
   }
 }
