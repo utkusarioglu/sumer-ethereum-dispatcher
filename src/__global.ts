0e8992bce60bc7b9e5447bfad01a47cf6b135df9
Utku Sarioglu <utkusarioglu@hotmail.com>
2021-12-31 20:39:00 +0300

    Update __global
    __global.ts
    - Update all env variable types to be optional (?)
    - Add `PORT` variable
    - Add some variables that are provided by docker and kubernetes.
      These relate to logging and bridge to kubernetes connections.

--- src/__global.ts
+++ src/__global.ts
@@ -3,12 +3,16 @@ import { LOG_LEVELS } from "_/services/logger/logger.constants";
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      NODE_ENV: "production" | "testing" | "development";
-      NETWORK: string; // homestead, goerli etc
-      KAFKA_BROKERS: string; // csv
-      HOSTNAME: string; // provided by docker or k8s
-      INFURA_API_KEY: string;
+      NODE_ENV?: "production" | "testing" | "development";
+      NETWORK?: string; // homestead, goerli etc
+      INFURA_API_KEY?: string;
       LOG_LEVEL?: keyof typeof LOG_LEVELS;
+      PORT?: string; // number
+
+      // k8s
+      HOSTNAME?: string; // provided by docker or k8s
+      SUMER_KAFKA_BOOTSTRAP_SERVICE_PORT?: string;
+      SUMER_KAFKA_BOOTSTRAP_SERVICE_HOST?: string;
     }
   }
 }
