59bd2f3ec72410eb97439a739674c0cc4a415e60
Utku Sarioglu <utkusarioglu@hotmail.com>
2021-12-31 20:36:05 +0300

    Update __config
    __config.ts
    - Add numeric value check for env.PORT
    - Update `KAFKA_BROKERS` to use const config value instead of .env
      value. This is done in preparation of these values being received
      from kubernetes variables. This is a requirement for bridge to
      kubernetes to work correctly.

--- src/__config.ts
+++ src/__config.ts
@@ -5,7 +5,8 @@ import { LOG_LEVEL_KEYS } from "./services/logger/logger.constants";
 assert(process.env.NODE_ENV, ".env.NODE_ENV is required");
 assert(process.env.INFURA_API_KEY, ".env.INFURA_API_KEY is required");
 assert(process.env.NETWORK, ".env.NETWORK is required");
-assert(process.env.KAFKA_BROKERS, ".env.KAFKA_BROKERS is required");
+assert(process.env.PORT, ".env.PORT is required")
+assert(!Number.isNaN(process.env.PORT), ".env.PORT is required to be a numeric value");
 assert(process.env.HOSTNAME, ".env.HOSTNAME is required");
 assert(
   process.env.LOG_LEVEL && LOG_LEVEL_KEYS.includes(process.env.LOG_LEVEL),
@@ -18,6 +19,9 @@ assert(
 export const NODE_ENV = process.env.NODE_ENV;
 export const INFURA_API_KEY = process.env.INFURA_API_KEY;
 export const NETWORK = process.env.NETWORK;
-export const KAFKA_BROKERS = process.env.KAFKA_BROKERS.split(",");
+export const KAFKA_BROKERS = [
+  "sumer-kafka-brokers.kafka:9092",
+];
 export const HOSTNAME = process.env.HOSTNAME;
 export const LOG_LEVEL = process.env.LOG_LEVEL;
+export const PORT = +process.env.PORT;
